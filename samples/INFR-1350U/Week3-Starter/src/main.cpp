fd2d0bd98aab1442c1adba3b9ae01e4f715a79af
Tyler <tyler.wong@ontariotechu.net>
2020-09-25 17:12:41 -0400

    Updated window title

--- samples/INFR-1350U/Week3-Starter/src/main.cpp
+++ samples/INFR-1350U/Week3-Starter/src/main.cpp
@@ -64,7 +64,7 @@ bool initGLFW() {
 	}
 
 	//Create a new GLFW window
-	window = glfwCreateWindow(800, 800, "INFR1350U", nullptr, nullptr);
+	window = glfwCreateWindow(800, 800, "Tyler Wong - 100750805", nullptr, nullptr);
 	glfwMakeContextCurrent(window);
 
 	// Set our window resized callback

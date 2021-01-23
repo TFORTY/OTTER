67ae76498af8eb7c714b2ac4243fe53840b59f2f
Tyler <tyler.wong@ontariotechu.net>
2021-01-23 11:55:26 -0500

    Game Sound Tutorial 1
    Setting up FMIOD

--- samples/GameSound/SampleStarter/src/Game.cpp
+++ samples/GameSound/SampleStarter/src/Game.cpp
@@ -12,7 +12,22 @@ float gameTime;
 
 void Init()
 {
-	
+	//Set up FMOD
+	AudioEngine& engine = AudioEngine::Instance();
+	engine.Init();
+	engine.LoadBank("Master");
+
+	//Play a music event
+	AudioEvent& music = engine.CreateEvent("music", "{b56cb9d2-1d47-4099-b80e-7d257b99a823}"); //Right click event in FMOD -> copy GUID
+	music.Play();
+
+	//Load a bus
+	engine.LoadBus("MusicBus", "{a5b53ded-d7b3-4e6b-a920-0b241ef6f268}"); //Right click bus in FMOD -> copy GUID
+
+	//Get ref to listener
+	AudioListener& listener = engine.GetListener();
+
+	//Can use this listener to change the player's 3D position
 }
 
 //------------------------------------------------------------------------
@@ -20,19 +35,34 @@ void Init()
 //------------------------------------------------------------------------
 void Update(float deltaTime)
 {
-
 	// Increment game time
 	gameTime += deltaTime;
 
-	
-	
-	// After 5 seconds go underwater
+	//Grab some references to our audio objects 
+	AudioEngine& audioEngine = AudioEngine::Instance();
+
+	AudioEvent& musicEvent = audioEngine.GetEvent("music"); //Make sure things strings match the names we gave them above
+	AudioBus& musicBus = audioEngine.GetBus("MusicBus");
+
+	//Pause the music for anticipation
 	if (gameTime > 4.0f)
 	{
-		// Do something
+		musicBus.SetPaused(true);
+	}
+	if (gameTime > 10.0f)
+	{
+		//Start playing again
+		musicBus.SetPaused(false);
+	}
+	if (gameTime > 15.0f)
+	{
+		//Change some parameters to make it sound funky
+		musicEvent.SetParameter("Underwater", 1.0f);
+		audioEngine.SetGlobalParameter("Timewarp", 0.0f);
 	}
 
-
+	//Make sure to update
+	audioEngine.Update();
 }
 
 
@@ -50,5 +80,6 @@ void Render()
 //------------------------------------------------------------------------
 void Shutdown()
 {
-
+	//Important! We don't want memory leaks!
+	AudioEngine::Instance().Shutdown();
 }
